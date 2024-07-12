3329631029aa1f21315536cf2394d0b656fd5545
xjx <85392378+JinxiaXie@users.noreply.github.com>
2024-07-12 22:10:54 +0800

    Update draw_success_precision.py

--- toolkit/visualization/draw_success_precision.py
+++ toolkit/visualization/draw_success_precision.py
@@ -10,7 +10,7 @@ def draw_success_precision(success_ret, name, videos, attr, precision_ret=None,
         norm_precision_ret=None, bold_name=None, axis=[0, 1]):
     # success plot
     fig, ax = plt.subplots()
-    ax.grid(b=True)
+    # ax.grid(b=True)
     ax.set_aspect(1)
     plt.xlabel('Overlap threshold')
     plt.ylabel('Success rate')
@@ -56,7 +56,7 @@ def draw_success_precision(success_ret, name, videos, attr, precision_ret=None,
     if precision_ret:
         # norm precision plot
         fig, ax = plt.subplots()
-        ax.grid(b=True)
+        # ax.grid(b=True)
         ax.set_aspect(50)
         plt.xlabel('Location error threshold')
         plt.ylabel('Precision')
@@ -100,7 +100,7 @@ def draw_success_precision(success_ret, name, videos, attr, precision_ret=None,
     # norm precision plot
     if norm_precision_ret:
         fig, ax = plt.subplots()
-        ax.grid(b=True)
+        # ax.grid(b=True)
         plt.xlabel('Location error threshold')
         plt.ylabel('Precision')
         if attr == 'ALL':
